2ba282b658ec9c0bdc9868f871b9d54993fe2be0
netman1221 <netman1221@gmail.com>
2016-01-08 13:31:54 +0900

    add files

---
+++ ko.php
@@ -0,0 +1,14 @@
+<?php
+$l_sys_login = "로그인";
+$l_sys_logout = "로그 아웃";
+$l_sys_download = "다운로드";
+$l_sys_upload = "업로드";
+$l_sys_uptime_7days = "1 주일 평균 가동률";
+$l_page_image = "사진 공유 서비스";
+$l_page_osu_sig = "osu! Signature generator";
+$l_page_title = "URL 단축 서비스";
+$l_page_server_status = "서버 정보";
+$l_page_Home = "Home";
+$l_url_src = "단축하고자하는 URL";
+$l_url_custom_url = "URL의 마지막을 사용자 정의하려는 경우 기입하십시오 (선택 사항)";
+$l_url_custom_name = "등록 이름을 사용자 지정할하려면 입력하십시오 (선택 사항)";
